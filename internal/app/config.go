47b72635355bd98c65a54e2a189df5c2ff332eb2
marvin <marvin.mitchell@amarbank.co.id>
2025-08-22 15:54:14 +0700

    update password for local postgres

--- internal/app/config.go
+++ internal/app/config.go
@@ -12,7 +12,7 @@ type Config struct {
 	DBHost         string `env:"DATA_PROTECTION_DB_ADDRESS" env-default:"localhost"`
 	DBPort         int    `env:"DATA_PROTECTION_DB_PORT" env-default:"5432"`
 	DBUser         string `env:"DATA_PROTECTION_DB_USER" env-default:"postgres"`
-	DBPassword     string `env:"DATA_PROTECTION_DB_PASSWORD" env-default:"Tunaiku2018"`
+	DBPassword     string `env:"DATA_PROTECTION_DB_PASSWORD" env-default:"password"`
 	DBName         string `env:"DATA_PROTECTION_DB_NAME" env-default:"go_machine"`
 	DBPoolSize     int    `env:"DATA_PROTECTION_DB_POOL_SIZE" env-default:"10"`
 	DBMaxRetries   int    `env:"DATA_PROTECTION_DB_MAX_RETRIES" env-default:"3"`
